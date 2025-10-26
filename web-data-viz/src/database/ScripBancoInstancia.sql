63bf9bc7ebd2f88fe749fcfa28fb842fcf3f593f
Lukascomka <lukas.souza@sptech.school>
2025-10-26 20:34:45 -0300

    modificacao banco de dados

--- web-data-viz/src/database/ScripBancoInstancia.sql
+++ web-data-viz/src/database/ScripBancoInstancia.sql
@@ -1,13 +1,6 @@
 CREATE DATABASE BlackScreen;
 USE BlackScreen;
 
-CREATE TABLE Empresa (
-    Id_Empresa INT AUTO_INCREMENT PRIMARY KEY ,
-    Nome_Empresa VARCHAR(255),
-    Cnpj VARCHAR(255) UNIQUE,
-    Fk_Endereco INT
-);
-
 CREATE TABLE Enderecos (
     Id_Endereco INT AUTO_INCREMENT PRIMARY KEY,
     Cep VARCHAR(9),
@@ -22,9 +15,22 @@ CREATE TABLE Enderecos (
     Complemento VARCHAR(200)
 );
 
-ALTER TABLE Empresa
-ADD CONSTRAINT FK_Empresa_Endereco
-FOREIGN KEY (Fk_Endereco) REFERENCES Enderecos(Id_Endereco);
+CREATE TABLE Empresa (
+    Id_Empresa INT AUTO_INCREMENT PRIMARY KEY ,
+    Nome_Empresa VARCHAR(255),
+    Cnpj VARCHAR(255) UNIQUE,
+    Fk_Endereco INT,
+    CONSTRAINT FK_Empresa_Endereco
+        FOREIGN KEY (Fk_Endereco) REFERENCES Enderecos(Id_Endereco)
+);
+
+CREATE TABLE Cargo (
+    Id_Cargo INT AUTO_INCREMENT PRIMARY KEY,
+    Nome_Cargo VARCHAR(255) NOT NULL,
+    Fk_Empresa INT,
+    CONSTRAINT FK_Cargo_Empresa
+        FOREIGN KEY (Fk_Empresa) REFERENCES Empresa(Id_Empresa)
+);
 
 CREATE TABLE Usuario (
     Id_Usuario INT AUTO_INCREMENT PRIMARY KEY,
@@ -32,8 +38,11 @@ CREATE TABLE Usuario (
     Email VARCHAR(255) UNIQUE,
     Senha VARCHAR(255),
     Fk_Empresa INT,
+    Fk_Cargo INT, -- Coluna Adicionada
     CONSTRAINT FK_Usuario_Empresa
-        FOREIGN KEY (Fk_Empresa) REFERENCES Empresa(Id_Empresa)
+        FOREIGN KEY (Fk_Empresa) REFERENCES Empresa(Id_Empresa),
+    CONSTRAINT FK_Usuario_Cargo -- Constraint Adicionada
+        FOREIGN KEY (Fk_Cargo) REFERENCES Cargo(Id_Cargo)
 );
 
 CREATE TABLE Caixa (
@@ -62,4 +71,21 @@ CREATE TABLE Parametros (
     Fk_Componente INT,
     CONSTRAINT FK_Parametros_Componentes
         FOREIGN KEY (Fk_Componente) REFERENCES Componentes(Id_Componente)
+);
+
+CREATE TABLE Permissao (
+    Id_Permissao INT AUTO_INCREMENT PRIMARY KEY,
+    Nome_Permissao VARCHAR(255) NOT NULL UNIQUE,
+    Descricao_Permissao VARCHAR(255)
+);
+
+CREATE TABLE CargoPermissao (
+    Fk_Cargo INT NOT NULL,
+    Fk_Permissao INT NOT NULL,
+    CONSTRAINT PK_CargoPermissao 
+        PRIMARY KEY (Fk_Cargo, Fk_Permissao),
+    CONSTRAINT FK_CargoPermissao_Cargo
+        FOREIGN KEY (Fk_Cargo) REFERENCES Cargo(Id_Cargo),
+    CONSTRAINT FK_CargoPermissao_Permissao
+        FOREIGN KEY (Fk_Permissao) REFERENCES Permissao(Id_Permissao)
 );
