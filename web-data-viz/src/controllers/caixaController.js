b997ad7fda72594b5bcf74f3fa3ebbded98f6b52
Lukascomka <lukas.souza@sptech.school>
2025-11-28 13:25:51 -0300

    altrando grafico, modelagem, css

--- web-data-viz/src/controllers/caixaController.js
+++ web-data-viz/src/controllers/caixaController.js
@@ -111,4 +111,25 @@ async function buscarPorMac(req, res) {
   }
 }
 
-module.exports = { listar, cadastrar, buscarPorMac, listarInfo, buscarChamado };
+async function buscarInfoCaixaBuckeet(req, res) {
+    var macaddress = req.params.macaddress; 
+    if (!macaddress) {
+        return res.status(400).send("MacAddress não informado");
+    }
+
+    caixaModel.buscarMacAddres(macaddress)
+        .then(function (resultado) {
+            if (resultado.length > 0) {
+                res.status(200).json(resultado);
+            } else {
+                res.status(204).send("Nenhum resultado encontrado!");
+            }
+        })
+        .catch(function (erro) {
+            console.log(erro);
+            console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
+            res.status(500).json(erro.sqlMessage);
+        });
+}
+
+module.exports = { listar, cadastrar, buscarPorMac, listarInfo, buscarChamado, buscarInfoCaixaBuckeet};
