5708df16b31805e65c6b03e27c8b7408ec2c87e2
HerculesSP <hercules.pereira@sptech.school>
2025-12-01 20:22:20 -0300

    adicionando gráfico de disponibilidade

--- web-data-viz/src/routes/cloud.js
+++ web-data-viz/src/routes/cloud.js
@@ -4,9 +4,12 @@ var router = express.Router();
 var cloudController = require("../controllers/cloudController");
 
 router.post("/enviar/:filename", function (req, res) {
-    cloudController.send(req, res);
-})
+  cloudController.send(req, res);
+});
 
-router.get('/visualizarRelatorio/:id', cloudController.acessarRelatorio);
+router.get("/visualizarRelatorio/:id", cloudController.acessarRelatorio);
+router.get("/buscaCSVgrafico", function (req, res) {
+  cloudController.buscaCSVgrafico(req, res);
+});
 
 module.exports = router;
